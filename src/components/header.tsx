18fc4cf34e36a0212ce95477bb83c517b0d94df1
MetaAngelDAO <96368688+MetaAngelDAO@users.noreply.github.com>
2021-12-21 15:37:56 +0300

    add link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,8 @@ import { VFC } from 'react';
 import s from './header.module.scss';
 
 const Header: VFC = () => {
-  return <header className={s.header}>META ANGEL DAO</header>;
+  return <header className={s.header}>
+    <a href="https://metaangeldao.com">META ANGEL DAO</a></header>;
 };
 
 export default Header;
