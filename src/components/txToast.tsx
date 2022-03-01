b951dee3e7026c9d36b93f36f429243ce48a88ab
chun224 <94545645+chun224@users.noreply.github.com>
2022-03-02 01:25:12 +0800

    Update txToast.tsx

--- src/components/txToast.tsx
+++ src/components/txToast.tsx
@@ -8,12 +8,12 @@ interface IProps {
 
 const TxToast: FC<IProps> = ({ txId }) => (
   <span>
-    Rinkeby scaner tx:{' '}
+    Polygon scaner tx:{' '}
     <a
       className={s.link}
       target="_blank"
       rel="noreferrer noopener"
-      href={`https://etherscan.io/tx/${txId}`}
+      href={`https://polygonscan.com//tx/${txId}`}
     >
       {`${txId.slice(0, 5)}...${txId.slice(-5)}`}
     </a>
