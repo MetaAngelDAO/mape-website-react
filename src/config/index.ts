b92ccc3969b1f05c09d5a0fe6c59767d1a644b88
Malik Dauda <blockchain0715@gmail.com>
2022-03-02 18:47:49 +1000

    Update the contract address

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ import ABI from './mint1.abi.json';
 
 export const password = 'mape';
 export const contractConfig = {
-  address: '0xAec6bFf4f7294E3e539a4c4B75B528a2B2b4C24A',
+  address: '0x1f8AeDF30f4885566a4F3A1b7Bf676B12F9d8eD1',
   abi: ABI as AbiItem[],
 };
